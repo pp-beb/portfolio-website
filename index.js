bfcf0deaaf45483a3056b7a72c518a2db9c18577
Prithak Koirala <prithakkoirala44@gmail.com>
2024-06-26 19:30:11 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+function changeHeaderColor() {
+    document.querySelector('header').style.backgroundColor = '#555';
+  }
+  
+  
+  function resetHeaderColor() {
+    document.querySelector('header').style.backgroundColor = '#333';
+  }
+  function changecontactColor() {
+    document.querySelector('div').style.backgroundColor = 'beige';
+  }
+  
+  
+  function resetcontactColor() {
+    document.querySelector('div').style.backgroundColor='#f7f7f7';
+  }
+  function changeexperiencesColor() {
+    document.getElementById('ok').style.backgroundColor = 'darkgrey';
+  }
+  function resetexperiencesColor() {
+    document.getElementById('ok').style.backgroundColor='#f7f7f7';
+  }
+  
+  function changeexperiences1Color() {
+    document.getElementById('ok1').style.backgroundColor='darkgrey';
+  }
+  
+  function resetexperiences1Color() {
+    document.getElementById('ok1').style.backgroundColor='#f7f7f7';
+  }
+  function changeexperiences2Color() {
+    document.getElementById('ok2').style.backgroundColor = 'darkgrey';
+  }
+  
+  
+  function resetexperiences2Color() {
+    document.getElementById('ok2').style.backgroundColor='#f7f7f7';
+  }
+  function changeexperiences3Color() {
+    document.getElementById('ok3').style.backgroundColor = 'darkgrey';
+  }
+  
+  
+  function resetexperiences3Color() {
+    document.getElementById('ok3').style.backgroundColor='#f7f7f7';
+  }
+  function changeexperiences4Color() {
+    document.getElementById('ok4').style.backgroundColor = 'darkgrey';
+  }
+  
+  
+  function resetexperiences4Color() {
+    document.getElementById('ok4').style.backgroundColor='#f7f7f7';
+  }
+  function changeexperiences5Color() {
+    document.getElementById('ok5').style.backgroundColor = 'darkgrey';
+  }
+  
+  
+  function resetexperiences5Color() {
+    document.getElementById('ok5').style.backgroundColor='#f7f7f7';
+  }
+  function changeexperiences6Color() {
+    document.getElementById('ok6').style.backgroundColor = 'darkgrey';
+  }
+  
+  
+  function resetexperiences6Color() {
+    document.getElementById('ok6').style.backgroundColor='#f7f7f7';
+  }
+  
+  function changeexperiences7Color() {
+    document.getElementById('ok7').style.backgroundColor = 'darkgrey';
+  }
+  
+  
+  function resetexperiences7Color() {
+    document.getElementById('ok7').style.backgroundColor='#f7f7f7';
+  }
+  
+
+ 
+
+
+
+
+
+ 
